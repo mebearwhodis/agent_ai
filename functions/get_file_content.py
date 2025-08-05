3372c3356146062b736b7b23a39348151ebe5641
mebearwhodis <mebearwhodis@gmail.com>
2025-08-05 16:14:22 +0200

    Agent correctly calling functions

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -4,14 +4,14 @@ from config import MAX_CHARS
 
 def get_file_content(working_directory, file_path):
     abs_working_dir = os.path.abspath(working_directory)
-    target_file = os.path.abspath(os.path.join(working_directory, file_path))
+    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
 
-    if not target_file.startswith(abs_working_dir):
+    if not abs_file_path.startswith(abs_working_dir):
         return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
-    if not os.path.isfile(target_file):
+    if not os.path.isfile(abs_file_path):
         return f'Error: File not found or is not a regular file: "{file_path}"'
     try:
-        with open(target_file, "r") as f:
+        with open(abs_file_path, "r") as f:
             file_content_string = f.read(MAX_CHARS)
             if os.path.getsize(abs_file_path) > MAX_CHARS:
                 file_content_string += (
