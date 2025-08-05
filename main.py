1fa4b5d0e834ba8818925aee7307426f9909edfa
mebearwhodis <mebearwhodis@gmail.com>
2025-08-05 15:06:30 +0200

    system prompt

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
+from prompts import system_prompt
+
 
 
 
@@ -42,6 +44,7 @@ def generate_content(client, messages, verbose):
     response = client.models.generate_content(
         model="gemini-2.0-flash-001", 
         contents=messages,
+        config=types.GenerateContentConfig(system_instruction=system_prompt)
         )
     if verbose:
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
